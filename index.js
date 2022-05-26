e225852a67f7fe7ce8460d73d2db80c571bc65c7
Jeihan Praja Yudanto <jeihan.yudanto@gmail.com>
2022-05-26 17:30:31 +0700

    feat: third commit

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const number1 = 10;
 const number2 = 20;
 let jumlah = number1 + number2;
 
-console.log("Jumlah dari " + number1 + " +  " + number2 + " adalah " + jumlah);
+console.log("Jumlah dari " + number1 + " + " + number2 + " adalah " + jumlah);
